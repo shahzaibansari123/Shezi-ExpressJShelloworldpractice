9e2c11f57485b47b1a237888e7fb1e57f64dcaf1
shahzaibansari123 <ansarishahzaib567@gmail.com>
2021-10-07 06:09:53 +0500

    heroku deployed

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const port = 3000
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.get('/profile', (req, res) => {
+    res.send('user profile')
+})
+app.post('/profile', (req, res) => {
+    res.send('profile created')
+})
+app.put('/profile', (req, res) => {
+    res.send('profile updated')
+})
+app.delete('/profile', (req, res) => {
+    res.send('profile deleted')
+})
+
+
+app.listen(port, () => {
+    console.log(`your app is ruuning at http://localhost:${port}`)
+})
